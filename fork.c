3abb24aa681c527bb8a9760eb2e441ffbd495135
NicolasDev153 <nikaberidze27@gmail.com>
2024-02-06 21:26:07 +0400

    fully implemented fork() system call.

--- fork.c
+++ fork.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h> // for pid_t data type and getpid function
+#include <sys/types.h>
 
 int main(){
-    pid_t child_pid;
+    pid_t child_pid = fork();
+    pid_t parentPid = getpid(); //this will be parent 
 
     //child process
     if(child_pid == 0){
-        //should output current PID and childs PID as 0
+        printf("### Child ###\nCurrent PID: %d and Child PID: %d\n",
+               parentPid, child_pid);
+               /*We could have also used getpid() directly instead of
+               creating parentPid*/
     } else {
-        //should output the parent one and then child
-        //in this case the child one has to be corresponding to the if's PID
+        printf("### Parent ###\nCurrent PID: %d and Child PID: %d\n",
+                parentPid, child_pid);
     }
-
 
     return 0;
 }
