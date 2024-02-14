92f765f437c91a7dac413d989935b3f34de6f793
Nikolozz993 <77141029+NicolasDev153@users.noreply.github.com>
2024-02-14 19:41:44 +0400

    sleep()
    Added sleep function for interval of 1 second.

---
+++ sleep_parent.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> // for pid_t data type and getpid function
+#include <sys/types.h>
+
+int main(){
+    pid_t child_pid = fork();
+    pid_t parentPid = getpid(); //this will be parent
+
+    //child process
+    if(child_pid == 0){
+        printf("### Child ###\nCurrent PID: %d and Child PID: %d\n",
+               parentPid, child_pid);
+               /*We could have also used getpid() directly instead of
+               creating parentPid*/
+    } else {
+        sleep(1); // Sleep for one second
+        printf("### Parent ###\nCurrent PID: %d and Child PID: %d\n",
+                parentPid, child_pid);
+    }
+
+    return 0;
+}
